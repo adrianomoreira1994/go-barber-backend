667697509ac75fae62dd0b3b4dbd529221d8dd85
Carlos Adriano da Silva Moreira <adrianomoreira2014@outlook.com>
2020-07-29 06:24:32 -0300

    iniciada modularização da aplicação

---
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -0,0 +1,50 @@
+import { getRepository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+import { compare } from 'bcryptjs';
+import authConfig from '../../../config/authConfig';
+
+import AppError from '../../../shared/errors/AppError';
+
+import User from '../infra/typeorm/entities/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new AppError('Incorret email/password combination', 401);
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError('Incorret email/password combination', 401);
+    }
+
+    const { expiresIn, secret } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
